9d477f5f6f21985453c069d8aeb2502736734152
caiaygelli@gmail.com <caiaygelli@gmail.com>
2025-04-27 19:30:07 -0400

    first push

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: false,
+    })
+  ],
+  build: {
+    cssCodeSplit: true,
+    rollupOptions: {
+      input: {
+        popup: 'src/main.jsx'
+      },
+      output: {
+        entryFileNames: 'assets/popup.js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
+});
